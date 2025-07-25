ab926d2069e3d37ebda7ffc93b4393d9bdea4b46
Berry-mas <junseo9145@gmail.com>
2025-07-25 16:36:29 +0900

    kyobo 및 yes24 병합

--- review_analysis/preprocessing/main.py
+++ review_analysis/preprocessing/main.py
@@ -4,13 +4,16 @@ from argparse import ArgumentParser
 from typing import Dict, Type
 from review_analysis.preprocessing.base_processor import BaseDataProcessor
 from review_analysis.preprocessing.kyobo_processor import KyoboProcessor
+from review_analysis.preprocessing.yes24_processor import yes24Processor
+
 
 
 # 모든 preprocessing 클래스를 예시 형식으로 적어주세요. 
 # key는 "reviews_사이트이름"으로, value는 해당 처리를 위한 클래스
 PREPROCESS_CLASSES: Dict[str, Type[BaseDataProcessor]] = {
-        "reviews_kyobo": KyoboProcessor,
-    # key는 크롤링한 csv파일 이름으로 적어주세요! ex. reviews_naver.csv -> reviews_naver
+        # "reviews_kyobo": KyoboProcessor,
+        "reviews_yes24": yes24Processor,
+    
 }
 
 REVIEW_COLLECTIONS = glob.glob(os.path.join("..","..","database", "reviews_*.csv"))
