e0410b76e7544aa6f678b7039fff47aa3dba820e
Berry-mas <junseo9145@gmail.com>
2025-08-07 21:56:06 +0900

    오류 수정

--- app/user/user_repository.py
+++ app/user/user_repository.py
@@ -3,16 +3,10 @@ from sqlalchemy.orm import Session
 from app.user.user_schema import User
 #from app.config import USER_DATA
 from sqlalchemy import text
-from database.mysql_connection import SessionLocal
 
 class UserRepository:
     def __init__(self, db: Session):
         self.db = db
-    
-    def _get_db(self) -> Session:
-        if self.db:
-            return self.db
-        return SessionLocal()
 
     # def _load_users(self) -> Dict[str, Dict]:
     #     try:
@@ -22,43 +16,31 @@ class UserRepository:
     #         raise ValueError("File not found")
 
     def get_user_by_email(self, email: str) -> Optional[User]:
-        db = self._get_db()
-        try:
-            result = db.execute(
-                text("SELECT * FROM users WHERE email = :email"),
-                {"email": email}
-            ).mappings().first()
+        result = self.db.execute(
+            text("SELECT * FROM users WHERE email = :email"),
+            {"email": email}
+        ).mappings().first()
 
-            if result is None:
-                return None
-            return User(**result)
-        finally:
-            db.close()
+        if result is None:
+            return None
+        return User(**result)
 
     def save_user(self, user: User) -> User:
-        db = self._get_db()
-        try:
-            db.execute(
-                text("REPLACE INTO users (username, email, password) VALUES (:username, :email, :password)"),
-                {
-                    "username": user.username,
-                    "email": user.email,
-                    "password": user.password
-                }
-            )
-            db.commit()
-            return user
-        finally:
-            db.close()
+        self.db.execute(
+            text("REPLACE INTO users (username, email, password) VALUES (:username, :email, :password)"),
+            {
+                "username": user.username,
+                "email": user.email,
+                "password": user.password
+            }
+        )
+        self.db.commit()
+        return user
 
     def delete_user(self, user: User) -> User:
-        db = self._get_db()
-        try:
-            db.execute(
-                text("DELETE FROM users WHERE email = :email"),
-                {"email": user.email}
-            )
-            db.commit()
-            return user
-        finally:
-            db.close()
+        self.db.execute(
+            text("DELETE FROM users WHERE email = :email"),
+            {"email": user.email}
+        )
+        self.db.commit()
+        return user
